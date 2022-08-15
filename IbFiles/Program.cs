ff9f22d6b7cca61e0f39c0a08a0f58e48535d3eb
IrishBruse <Econn50@outlook.com>
2022-08-15 04:10:20 +0100

    Imgui Init

--- IbFiles/Program.cs
+++ IbFiles/Program.cs
@@ -1,36 +1,56 @@
-﻿using Silk.NET.Maths;
+﻿using System.Drawing;
+
+using ImGuiNET;
+
+using Silk.NET.Input;
+using Silk.NET.Maths;
+using Silk.NET.OpenGL;
+using Silk.NET.OpenGL.Extensions.ImGui;
 using Silk.NET.Windowing;
 
 WindowOptions windowOptions = WindowOptions.Default;
 windowOptions.Size = new Vector2D<int>(800, 600);
-windowOptions.ShouldSwapAutomatically = false;
 windowOptions.Title = "Test";
 windowOptions.VSync = true;
-IWindow window = Window.Create(windowOptions);
+using IWindow window = Window.Create(windowOptions);
+
+ImGuiController controller = default;
+GL gl = default;
+IInputContext inputContext = default;
 
 // Events
-window.Load += OnLoad;
-window.Render += OnRender;
-window.Closing += OnClosing;
-window.Resize += OnResize;
+window.Load += () => controller = new ImGuiController(gl = window.CreateOpenGL(), window, inputContext = window.CreateInput());
+
+window.Render += (double delta) =>
+{
+    // Make sure ImGui is up-to-date
+    controller.Update((float)delta);
+
+    // This is where you'll do any rendering beneath the ImGui context
+    // Here, we just have a blank screen.
+    gl.ClearColor(Color.FromArgb(255, (int)(.45f * 255), (int)(.55f * 255), (int)(.60f * 255)));
+    gl.Clear((uint)ClearBufferMask.ColorBufferBit);
+
+    // This is where you'll do all of your ImGUi rendering
+    // Here, we're just showing the ImGui built-in demo window.
+    ImGui.ShowDemoWindow();
+
+    // Make sure ImGui renders too!
+    controller.Render();
+};
+
+window.Closing += () =>
+{                // Dispose our controller first
+    controller?.Dispose();
+
+    // Dispose the input context
+    inputContext?.Dispose();
+
+    // Unload OpenGL
+    gl?.Dispose();
+};
+
+
+window.FramebufferResize += (s) => gl.Viewport(s);
 
 window.Run();
-
-void OnLoad()
-{
-}
-
-
-void OnRender(double dt)
-{
-}
-
-
-void OnClosing()
-{
-}
-
-
-void OnResize(Vector2D<int> size)
-{
-}
