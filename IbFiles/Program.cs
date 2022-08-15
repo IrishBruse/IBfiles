d405e27286987241ccc708a8ef86cf5bf19dd423
IrishBruse <Econn50@outlook.com>
2022-08-15 03:55:34 +0100

    first commit

---
+++ IbFiles/Program.cs
@@ -0,0 +1,36 @@
+﻿using Silk.NET.Maths;
+using Silk.NET.Windowing;
+
+WindowOptions windowOptions = WindowOptions.Default;
+windowOptions.Size = new Vector2D<int>(800, 600);
+windowOptions.ShouldSwapAutomatically = false;
+windowOptions.Title = "Test";
+windowOptions.VSync = true;
+IWindow window = Window.Create(windowOptions);
+
+// Events
+window.Load += OnLoad;
+window.Render += OnRender;
+window.Closing += OnClosing;
+window.Resize += OnResize;
+
+window.Run();
+
+void OnLoad()
+{
+}
+
+
+void OnRender(double dt)
+{
+}
+
+
+void OnClosing()
+{
+}
+
+
+void OnResize(Vector2D<int> size)
+{
+}
---
+++ Readme.md
@@ -0,0 +1,7 @@
+# File manager
+
+## Planned Features
+- Similar to windows file manager
+- Simalar to dolphin linux file manager
+- Customize enviroment variables (i need to do this often)
+- Customizable right click menu
