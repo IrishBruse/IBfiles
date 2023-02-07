b246d62ac84eafe4afe76ffe28c22c263d1891bd
IrishBruse <Econn50@outlook.com>
2023-02-07 14:02:50 +0000

    Fix vscode not working because of it not being run in the shell

--- IBfiles/Logic/CommandHandler.cs
+++ IBfiles/Logic/CommandHandler.cs
@@ -1,6 +1,5 @@
 namespace IBfiles.Logic;
 
-using System;
 using System.Diagnostics;
 
 public static class CommandHandler
@@ -8,39 +7,16 @@ public static class CommandHandler
     public static void Run(string file, string args, string input)
     {
         string arguments = args.Replace("%1", input);
-        Process proc = new()
-        {
-            StartInfo = new()
-            {
-                RedirectStandardError = true,
-                RedirectStandardOutput = true
-            }
-        };
+        Process proc = new();
 
         proc.StartInfo.FileName = file;
         proc.StartInfo.Arguments = arguments;
         proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
 
-        proc.StartInfo.UseShellExecute = false;
+        proc.StartInfo.UseShellExecute = true;
 
         proc.Start();
 
         proc.WaitForExit();
-
-        string stdErr = proc.StandardError.ReadToEnd();
-
-        if (!string.IsNullOrEmpty(stdErr))
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.Error.WriteLine(stdErr);
-            Console.ResetColor();
-        }
-
-        string stdOut = proc.StandardOutput.ReadToEnd();
-
-        if (!string.IsNullOrEmpty(stdOut))
-        {
-            Console.WriteLine(stdOut);
-        }
     }
 }
